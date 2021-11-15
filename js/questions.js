7604aca19237584d568d92b6c5763fac60e0182f
ItszVinTech <77338381+ItszVinTech@users.noreply.github.com>
2021-11-15 20:36:11 +0000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "📅 What day comes after Sunday?",
+    answer: "Monday",
+    options: [
+      "Monday",
+      "Tuesday",
+      "Wednnesday",
+      "Pancake Day 🍥"
+    ]
+  },
+    {
+    numb: 2,
+    question: "🍀 What colour is grass? (In Irish 😉)",
+    answer: "Glas 💚",
+    options: [
+      "Gorm 💙",
+      "Glas 💚",
+      "Buí 💛",
+      "Oráiste 🧡"
+    ]
+  },
+    {
+    numb: 3,
+    question: "➕ What is 5+4?",
+    answer: "9",
+    options: [
+      "1",
+      "2",
+      "9",
+      "2021"
+    ]
+  },
+    {
+    numb: 4,
+    question: "🇮🇪 What is the capital of Ireland?",
+    answer: "Dublin",
+    options: [
+      "Dublin",
+      "France",
+      "Rome",
+      "Beijing"
+    ]
+  },
+    {
+    numb: 5,
+    question: "🏫 What school  do you go to?",
+    answer: "Queen Of Angels",
+    options: [
+      "Queen Of Angels",
+      "I dont even go to school!",
+      "How should I know?",
+      "Ummm..."
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
